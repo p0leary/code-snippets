4117454227b30cb9de97e20c3e9db9487bfa2c93
p0leary <oleary.jpatrick@gmail.com>
2018-11-15 00:46:48 +0000

    Update pyspark_snippets.py

--- pyspark_snippets.py
+++ pyspark_snippets.py
@@ -1,2 +1,4 @@
-# Comand to generate a simple dataframe in pyspark interpreter
+# Comands to generate a simple dataframe in pyspark interpreter
 df = sqlContext.createDataFrame([('a', 'bob'), ('a', 'susan')], ['col1', 'col2'])
+df = sqlContext.createDataFrame([('1', 12, '1'), ('2', 13, '1')], ['col1', 'col2', 'col3'])
+
